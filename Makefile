f26f3df9171e4aa9cb8fc9c0d3f5fbbb11c12608
Xin Liu <xin.liu@oss.cipunited.com>
2023-03-20 18:22:36 +0800

    enter protect mode

--- Makefile
+++ Makefile
@@ -19,26 +19,30 @@ CP=cp
 
 .PHONY: all
 
-all: $(boot) $(loader) 
+all: $(mnt) $(boot) $(img) $(loader) 
 	@echo "succeed!"
 
-$(img): $(boot)
-	@dd if=/dev/zero of=$(img) bs=512 count=2880
-	@$(MKFS) -F 12 -n "Aaron" $(img) > /dev/null
-	@dd if=$(boot) of=$(img) bs=512 count=1 conv=notrunc
+$(img): 
+	@dd if=/dev/zero of=$@  bs=512 count=2880
+	@$(MKFS) -F 12 -n "Aaron" $@ > /dev/null
+	@dd if=$(boot) of=$@ bs=512 count=1 conv=notrunc
 
 $(boot): $(bootsrc)
-	$(NASM) -g $(bootsrc) -o $(boot)
+	$(NASM) -g $< -o $@
 
-$(loader): $(loadersrc) $(mnt) $(img)
-	@$(NASM) $(loadersrc) -o $(loader)
+$(loader): $(bootsrc) $(loadersrc)
+	@$(NASM) $< -o $@
 	@sudo $(MOUNT) $(img) $(mnt)
 	@sudo $(CP) $(loader) $(mnt)
 	@sudo $(UMOUNT) $(mnt)
 $(mnt):
 	$(MKDIR) mnt
 	
-.PHONY: clean
+.PHONY: clean rebuild
+
+rebuild:
+	$(MAKE) clean
+	$(MAKE) all
 
 clean:
 	$(RM) -rf $(boot) $(loader) $(img) $(mnt)
--- loader.asm
+++ loader.asm
@@ -1,24 +1,75 @@
+%include "include.asm"
+
 org 0x9000
 
-start:
-	mov si, msg
+jmp CODE16_SEG
 
-print_String:
-	mov al, [si]
-	add si, 1
-	cmp al, 0x00
-	je end_Print
-	mov ah, 0x0E
-	mov bx, 0x0F
-	int 0x10
-	jmp print_String
+[section .gdt]
+; GDT definition
+;						 		段基址，					段界限，					段属性
+GDT_ENTRY			:		Descriptor	0,				0,						0
+CODE32_DESC			:		Descriptor	0, 				Code32SegLen  - 1,		DA_C + DA_32
 
-end_Print:
-	hlt
-	jmp end_Print
+GdtLen	equ		$ - GDT_ENTRY
 
-msg:
-	db 0x0a, 0x0a
-	db "Hello, Aaron.OS!"
-	db 0x0a, 0x0a
-	db 0x00
+GdtPtr:
+	dw GdtLen
+	dd 0
+
+;GDT Selector
+Code32Selector		equ (0x0001 << 3) + SA_TIG + SA_RPL0
+
+;end of [section .gdt]
+
+[section .s16]
+[bits 16]
+CODE16_SEG:
+	mov ax, cs
+	mov ds, ax
+	mov es, ax
+	mov ss, ax
+	mov sp, 0x7c00
+	
+	; initialize GDT for 32 bits code segment
+	mov eax, 0
+	mov ax, cs
+	shl eax, 4
+	add eax, CODE32_SEG
+	mov [CODE32_DESC + 2], ax
+	shr eax, 16
+	mov byte [CODE32_DESC + 4], al
+	mov byte [CODE32_DESC + 7], ah
+    
+	; initialize GDT pointer struct
+	mov eax, 0
+	mov ax, cs
+	shl eax, 4
+	add eax, GDT_ENTRY
+	mov [GdtPtr + 2], eax
+	
+	; 1.load GDT
+	lgdt [GdtPtr]
+	
+	; 2.close interrupt
+	cli
+	
+	; 3.open A20 地址线
+	in al, 0x92
+	or al, 00000010b
+	out 0x92, al
+	
+	; 4.enter protect mode 通知cpu进入保护模式
+	mov eax, cr0
+	or eax, 0x01
+	mov cr0, eax
+	
+	; 5.jump to 32 bits code 
+	jmp dword Code32Selector : 0
+	
+[section .s32]
+[bits 32]
+CODE32_SEG:
+	mov ax, 0
+	jmp CODE32_SEG
+
+Code32SegLen	equ $ - CODE32_SEG
