1ff02df8eb294e31a3daefa3f230240cce810526
Xin Liu <xin.liu@oss.cipunited.com>
2023-03-24 16:55:28 +0800

    add a ldt segment 	jmp to ldt seg print string, and back to real mode

--- loader.asm
+++ loader.asm
@@ -14,6 +14,7 @@ STACK32_DESC		:		Descriptor	0, 				TopOfStack32,			DA_DRW + DA_32
 DISPLAY_DESC		:		Descriptor	0xB8000, 		0x07FFF,				DA_DRWA + DA_32
 CODE16_DESC			:		Descriptor	0, 				0xFFFF,					DA_C
 UPDATE_DESC			:		Descriptor	0, 				0xFFFF,					DA_DRW
+TASK_A_LDT_DESC		:		Descriptor	0, 				TaskALdtLen - 1,		DA_LDT
 
 GdtLen	equ		$ - GDT_ENTRY
 
@@ -25,10 +26,10 @@ GdtPtr:
 Code32Selector		equ (0x0001 << 3) + SA_TIG + SA_RPL0
 Data32Selector		equ (0x0002 << 3) + SA_TIG + SA_RPL0
 Stack32Selector		equ (0x0003 << 3) + SA_TIG + SA_RPL0
-Display32Selector	equ (0x0004 << 3) + SA_TIG + SA_RPL0
+DisplaySelector		equ (0x0004 << 3) + SA_TIG + SA_RPL0
 Code16Selector		equ (0x0005 << 3) + SA_TIG + SA_RPL0
 UpdateSelector		equ (0x0006 << 3) + SA_TIG + SA_RPL0
-
+TaskALdtSecector	equ (0x0007 << 3) + SA_TIG + SA_RPL0
 ;end of [section .gdt]
 
 TopOfStack16    equ 0x7c00
@@ -57,7 +58,6 @@ ENTRY_SEG:
 	mov [BACK_TO_REAL_MODE + 3], ax
 	
 	; initialize GDT for 32 bits code segment
-	
 	mov esi, CODE32_SEG
 	mov edi, CODE32_DESC
 	
@@ -81,12 +81,32 @@ ENTRY_SEG:
 	
 	call initDescItem
 	
+	mov esi, TASK_A_LDT_ENTRY
+	mov edi, TASK_A_LDT_DESC
+	
+	call initDescItem
+	
+	mov esi, TASK_A_CODE32_SEG
+	mov edi, TASK_A_CODE32_DESC
+	
+	call initDescItem
+	
+	mov esi, TASK_A_DATA32_SEG
+	mov edi, TASK_A_DATA32_DESC
+	
+	call initDescItem
+	
+	mov esi, TASK_A_STACK32_SEG
+	mov edi, TASK_A_STACK32_DESC
+	
+	call initDescItem
+	
 	; initialize GDT pointer struct
 	mov eax, 0
-	mov ax, cs
+	mov ax, ds
 	shl eax, 4
 	add eax, GDT_ENTRY
-	mov [GdtPtr + 2], eax
+	mov dword [GdtPtr + 2], eax
 	
 	; 1.load GDT
 	lgdt [GdtPtr]
@@ -186,7 +206,7 @@ CODE32_SEG:
 	mov eax, TopOfStack32
 	mov esp, eax
 	; 设置显存选择子
-	mov ax, Display32Selector
+	mov ax, DisplaySelector
 	mov gs, ax
 	
 	mov ebp, AAOS_OFFSET
@@ -203,7 +223,11 @@ CODE32_SEG:
 	
 	call print_String
 	
-	jmp Code16Selector : 0
+	mov ax, TaskALdtSecector
+	
+	lldt ax
+	
+	jmp TaskACode32Selector : 0
 	
 ; ds:ebp    --> string address
 ; bx        --> attribute
@@ -255,3 +279,116 @@ STACK32_SEG:
 	
 Stack32SegLen equ $ - STACK32_SEG
 TopOfStack32  equ Stack32SegLen - 1
+
+;局部描述段:
+; ==========================================
+;
+;            Task A Code Segment 
+;
+; ==========================================
+
+[section .task-a-ldt]
+; Task A LDT definition 局部描述符
+;								段基址,						段界限,						段属性
+TASK_A_LDT_ENTRY:
+TASK_A_CODE32_DESC		:		Descriptor	0,			TaskACode32SegLen - 1,			DA_C + DA_32
+TASK_A_DATA32_DESC		:		Descriptor	0,			TaskAData32SegLen - 1,			DA_DR + DA_32
+TASK_A_STACK32_DESC		:		Descriptor	0,			TaskAStack32SegLen - 1,			DA_DRW + DA_32
+
+TaskALdtLen equ $ - TASK_A_LDT_ENTRY
+
+; Task A LDT Selector
+TaskACode32Selector  equ   (0x0000 << 3) + SA_TIL + SA_RPL0
+TaskAData32Selector  equ   (0x0001 << 3) + SA_TIL + SA_RPL0
+TaskAStack32Selector equ   (0x0002 << 3) + SA_TIL + SA_RPL0
+
+[section .task-a-dat]
+[bits 32]
+TASK_A_DATA32_SEG:
+	TASK_A_STRING			db  "This is Task A!", 0
+	TASK_A_STRING_OFFSET	equ TASK_A_STRING - $$
+
+TaskAData32SegLen  equ  $ - TASK_A_DATA32_SEG
+
+[section .task-a-gs]
+[bits 32]
+TASK_A_STACK32_SEG:
+	times 1024 db 0 
+	
+TaskAStack32SegLen	equ	$ - TASK_A_STACK32_SEG
+TaskATopOfStack32	equ	TaskAStack32SegLen - 1
+
+; 局部描述段的代码段
+[section .task-a-s32]
+[bits 32]
+TASK_A_CODE32_SEG:
+	mov ax, TaskAData32Selector
+	mov ds, ax
+	
+	mov ax, TaskAStack32Selector
+	mov ss, ax
+	
+	mov eax, TaskATopOfStack32
+	mov esp, eax
+	
+	mov ax, DisplaySelector
+	mov gs, ax
+	
+	mov ebp, TASK_A_STRING_OFFSET
+	mov bx, 0x0C
+	mov dh, 14
+	mov dl, 31
+	
+	call taskPrintString
+	
+	jmp Code16Selector : 0
+	
+; ds:ebp    --> string address
+; bx        --> attribute
+; dx        --> dh : row, dl : col
+taskPrintString:
+	push ebp
+	push eax
+	push edi
+	push cx
+	push dx
+	
+task_Print:
+	mov cl, [ds:ebp]
+	cmp cl, 0
+	je end_task_Print
+	; 每行80个字符，乘以dh(行)，得到所设置的行数的起始位置
+	mov eax, 80
+	mul dh
+	; 将列数加入al中，此时eax存放的就是要打印的位置
+	add al, dl
+	; 显卡文本显示，低位设置字符，高位设置显示属性
+	; 通过左移1位，得到乘以2后的结果
+	shl eax, 1
+	mov edi, eax
+	; 将ax低位设置字符，ax高位设置显示属性
+	mov ah, bl
+	mov al, cl
+	mov [gs:edi], ax
+	inc ebp
+	inc dl
+	jmp task_Print
+	
+end_task_Print:
+	pop dx
+	pop cx
+	pop edi
+	pop eax
+	pop ebp
+    
+	ret
+	
+TaskACode32SegLen	equ $ - TASK_A_CODE32_SEG
+
+
+
+
+
+
+
+
