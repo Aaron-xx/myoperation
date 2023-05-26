d01ae43afb08a840361093d65d6835abd0510992
Xin Liu <xin.liu@oss.cipunited.com>
2023-05-26 17:54:00 +0800

    Init for kernel
    Preper to run in kernel from bootloader

--- include.asm
+++ include.asm
@@ -8,26 +8,6 @@ DA_CR    equ    0x9A		; 可执行可读代码段
 DA_CCO   equ    0x9C		; 只执行一致代码段
 DA_CCOR  equ    0x9E		; 可执行可读一致代码段
 
-DA_LIMIT_4K    EQU       0x8000 ; 以4K(一页)为单位
-
-; Special Attribute
-DA_LDT   equ    0x82		; 局部描述符段
-
-; Segment Privilege		段的权限等级
-DA_DPL0		equ	  0x00		; DPL = 0
-DA_DPL1		equ	  0x20		; DPL = 1
-DA_DPL2		equ	  0x40		; DPL = 2
-DA_DPL3		equ	  0x60		; DPL = 3
-
-; Special Attribute
-DA_LDT		equ	0x82
-
-DA_TaskGate	equ	0x85	; 任务门类型值
-DA_386TSS	equ	0x89	; 可用 386 任务状态段类型值
-DA_386CGate	equ	0x8C	; 386 调用门类型值
-DA_386IGate	equ	0x8E	; 386 中断门类型值
-DA_386TGate	equ	0x8F	; 386 陷阱门类型值
-
 ; Selector Attribute
 SA_RPL0    equ    0
 SA_RPL1    equ    1
@@ -36,12 +16,6 @@ SA_RPL3    equ    3
 
 SA_TIG    equ    0			; GDT 选择子
 SA_TIL    equ    4			; LDT 选择子
-
-PG_P      equ    1    ; 页存在属性位
-PG_RWR    equ    0    ; R/W 属性位值, 读/执行
-PG_RWW    equ    2    ; R/W 属性位值, 读/写/执行
-PG_USS    equ    0    ; U/S 属性位值, 系统级
-PG_USU    equ    4    ; U/S 属性位值, 用户级
 
 ; 描述符
 ; usage: Descriptor Base, Limit, Attr
@@ -55,16 +29,3 @@ PG_USU    equ    4    ; U/S 属性位值, 用户级
     dw    ((%2 >> 8) & 0xF00) | (%3 & 0xF0FF) ; 属性1 + 段界限2 + 属性2
     db    (%1 >> 24) & 0xFF                   ; 段基址3
 %endmacro                                     ; 共 8 字节
-
-; 门
-; usage: Gate Selector, Offset, DCount, Attr
-;        Selector:  dw
-;        Offset:    dd
-;        DCount:    db
-;        Attr:      db
-%macro Gate 4
-    dw    (%2 & 0xFFFF)                      ; 偏移地址1
-    dw    %1                                 ; 选择子
-    dw    (%3 & 0x1F) | ((%4 << 8) & 0xFF00) ; 属性
-    dw    ((%2 >> 16) & 0xFFFF)              ; 偏移地址2
-%endmacro 
--- loader.asm
+++ loader.asm
@@ -21,13 +21,6 @@ CODE32_DESC			:		Descriptor	0, 				Code32SegLen  - 1,		DA_C + DA_32
 DATA32_DESC			:		Descriptor	0, 				Data32SegLen  - 1,		DA_DRW + DA_32
 STACK32_DESC		:		Descriptor	0, 				TopOfStack32,			DA_DRW + DA_32
 DISPLAY_DESC		:		Descriptor	0xB8000, 		0x07FFF,				DA_DRWA + DA_32
-PAGE_DIR_DESC0		:		Descriptor	PageDirBase0,	4095,					DA_DRW + DA_32
-PAGE_TBL_DESC0		:		Descriptor	PageTblBase0,	1023,					DA_DRW + DA_LIMIT_4K + DA_32
-PAGE_DIR_DESC1		:		Descriptor	PageDirBase1,	4095,					DA_DRW + DA_32
-PAGE_TBL_DESC1		:		Descriptor	PageTblBase1,	1023,					DA_DRW + DA_LIMIT_4K + DA_32
-FLAT_MODE_RW_DESC	:		Descriptor	0,				0xFFFFF,				DA_DRW + DA_LIMIT_4K + DA_32
-FUNC32_DESC			:		Descriptor	0,				Func32SegLen - 1,		DA_DR + DA_LIMIT_4K + DA_32
-FLAT_MODE_C_DESC	:		Descriptor	0,				0xFFFFF,				DA_C + DA_LIMIT_4K + DA_32
 
 ; GDT end
 
@@ -42,13 +35,6 @@ Code32Selector			equ (0x0001 << 3) + SA_TIG + SA_RPL0
 Data32Selector			equ (0x0002 << 3) + SA_TIG + SA_RPL0
 Stack32Selector			equ (0x0003 << 3) + SA_TIG + SA_RPL0
 DisplaySelector			equ (0x0004 << 3) + SA_TIG + SA_RPL0
-PageDirSelector0		equ (0x0005 << 3) + SA_TIG + SA_RPL0
-PageTblSelector0		equ (0x0006 << 3) + SA_TIG + SA_RPL0
-PageDirSelector1		equ (0x0007 << 3) + SA_TIG + SA_RPL0
-PageTblSelector1		equ (0x0008 << 3) + SA_TIG + SA_RPL0
-FlatModeSelector		equ (0x0009 << 3) + SA_TIG + SA_RPL0
-Func32Selector			equ (0x000A << 3) + SA_TIG + SA_RPL0
-FlatCModeSelector		equ (0x000B << 3) + SA_TIG + SA_RPL0
 ;end of [section .gdt]
 	
 TopOfStack16    equ 0x7c00
@@ -92,11 +78,6 @@ ENTRY_SEG:
 	
 	call initDescItem
 
-	mov esi, FUNC32_SEG
-	mov edi, FUNC32_DESC
-	
-	call initDescItem
-	
 	; initialize GDT pointer struct
 	mov eax, 0
 	mov ax, ds
@@ -144,51 +125,15 @@ initDescItem:
 	
 	ret
 
-[section .func]
-[bits 32]
-FUNC32_SEG:
-; cx --> n
-; return:
-;     eax --> n * n
-Sqr:
-	mov eax, 0
-    
-	mov ax, cx
-	mul cx
-    
-	retf
-    
-SqrLen  equ  $ - Sqr
-SqrFunc equ  Sqr - $$
-
-; cx --> n
-; return:
-;     eax --> 1 + 2 + 3 + ... + n
-Acc:
-	push cx
-    
-	mov eax, 0
-    
-accLoop:
-	add ax, cx
-	loop accLoop
-    
-	pop cx
-    
-	retf
-    
-AccLen  equ  $ - Acc
-AccFunc equ  Acc - $$
-
-Func32SegLen  equ  $ - FUNC32_SEG
-
-
 [section .s32]
 [bits 32]
 CODE32_SEG:
 	; 设置显存选择子
 	mov ax, DisplaySelector
 	mov gs, ax
+
+	mov ax, Data32Selector
+	mov ds, ax
 
 	; 设置栈段选择子
 	mov ax, Stack32Selector
@@ -197,222 +142,14 @@ CODE32_SEG:
 	mov eax, TopOfStack32
 	mov esp, eax
 
-	mov ax, Func32Selector
-	mov ds, ax
+	mov ebp, AARON_OFFSET
+	mov ebx, 0x0C
+	mov dh, 13
+	mov dl, 33
 
-	mov ax, FlatModeSelector
-	mov es, ax
-
-	mov esi, SqrFunc
-	mov edi, TargetAddr1
-	mov ecx, SqrLen
-
-	call memCpy
-
-	mov esi, AccFunc
-	mov edi, TargetAddr2
-	mov ecx, AccLen
-
-	call memCpy
-
-	mov eax, PageDirSelector0
-	mov ebx, PageTblSelector0
-	mov ecx, PageTblBase0
-	
-	call initPageTable
-
-	mov eax, PageDirSelector1
-	mov ebx, PageTblSelector1
-	mov ecx, PageTblBase1
-
-	call initPageTable
-
-	mov eax, SourceAddr
-	mov ebx, TargetAddr1
-	mov ecx, SqrLen
-
-	call mapAddress
-
-	mov eax, SourceAddr
-	mov ebx, TargetAddr2
-	mov ecx, AccLen
-
-	call mapAddress
-
-	mov eax, PageDirBase0
-
-	call switchPageTable
-
-	mov ecx, 100
-
-	call FlatCModeSelector : TargetAddr1
-
-	mov eax, PageDirBase1
-
-	call switchPageTable
-
-	mov ecx, 100
-
-	call FlatCModeSelector : TargetAddr2
+	call printString
 
 	jmp $
-
-; es  --> flat mode
-; eax --> virtual address
-; ebx --> target  address
-; ecx --> page directory base
-mapAddress:
-	push edi
-	push esi 
-	push eax    ; [esp + 8]
-	push ebx    ; [esp + 4]
-	push ecx    ; [esp]
-    
-	; 1. 取虚地址高 10 位， 计算子页表在页目录中的位置 ==> eax
-	mov eax, [esp + 8]
-	shr eax, 22
-	and eax, 1111111111b
-	shl eax, 2
-    
-	; 2. 取虚地址中间 10 位， 计算物理地址在子页表中的位置 ==> ebx
-	mov ebx, [esp + 8]
-	shr ebx, 12
-	and ebx, 1111111111b
-	shl ebx, 2
-    
-	; 3. 取子页表起始地址
-	mov esi, [esp]
-	add esi, eax
-	mov edi, [es:esi]
-	and edi, 0xFFFFF000
-    
-	; 4. 将目标地址写入子页表的对应位置
-	add edi, ebx
-	mov ecx, [esp + 4]
-	and ecx, 0xFFFFF000
-	or  ecx, PG_P | PG_USU | PG_RWW
-	mov [es:edi], ecx
-    
-	pop ecx
-	pop ebx
-	pop eax
-	pop esi
-	pop edi
-    
-	ret
-
-; es     --> flat mode selector
-; ds:esi --> source
-; es:edi --> destination
-; ecx    --> length
-memCpy:
-    push esi
-    push edi
-    push ecx
-    push ax
-    
-    cmp esi, edi
-    
-    ja btoe
-    
-    add esi, ecx
-    add edi, ecx
-    dec esi
-    dec edi
-    
-    jmp etob
-    
-btoe:
-    cmp ecx, 0
-    jz done
-    mov al, [ds:esi]
-    mov byte [es:edi], al
-    inc esi
-    inc edi
-    dec ecx
-    jmp btoe
-    
-etob: 
-    cmp ecx, 0
-    jz done
-    mov al, [ds:esi]
-    mov byte [es:edi], al
-    dec esi
-    dec edi
-    dec ecx
-    jmp etob
-
-done:   
-    pop ax
-    pop ecx
-    pop edi
-    pop esi
-    ret
-
-; eax --> page dir base selector
-; ebx --> page table base selector 
-; ecx --> page table base
-; 初始化页表
-initPageTable:
-	push es
-	push eax  ; [esp + 12]
-	push ebx  ; [esp + 8]
-	push ecx  ; [esp + 4]
-	push edi  ; [esp]
-
-	mov es, ax
-	mov ecx, 1024	; 1k个子页表
-	mov edi, 0
-	mov eax, [esp + 4]
-	or eax, PG_P | PG_USU | PG_RWW
-
-	cld
-
-stdir:
-	; 传送指令 ,stosb,stosbw,stosd,将al,ax,eax 的值存储到edi指向的内存单元中
-	; 使用 方向标志位cld或者std，edi 增加或减少
-	stosd
-	add eax, 4096
-	loop stdir
-
-	mov ax, [esp + 8]
-	mov es, ax
-	mov ecx, 1024 * 1024	; 1M个页
-	mov edi, 0
-	mov eax, PG_P | PG_USU | PG_RWW
-
-	cld
-
-sttlb:
-	stosd
-	add eax, 4096
-	loop sttlb
-
-	pop edi
-	pop ecx
-	pop ebx
-	pop eax
-	pop es
-
-	ret
-
-; eax --> page directory base
-switchPageTable:
-	push eax
-
-	mov eax, cr0
-	and eax, 0x7FFFFFFF
-	mov cr0, eax
-
-	mov eax, [esp]
-	mov cr3, eax
-	mov eax, cr0
-	or eax, 0x80000000
-	mov cr0, eax
-
-	pop eax
-
-	ret
 	
 ; ds:ebp    --> string address
 ; bx        --> attribute
